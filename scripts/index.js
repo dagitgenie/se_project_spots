b3f808fda0a4fe793e9382353c04519ab2234648
dagitgenie <ernest.git.projects@gmail.com>
2026-04-19 21:14:40 -0400

    Add save features to modals and create smooth transition in the open and close states of modals

--- scripts/index.js
+++ scripts/index.js
@@ -3,60 +3,117 @@
 const initialCards = [
   {
     name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
   {
     name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
   },
   {
     name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
   },
   {
     name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
   {
     name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
   },
   {
     name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
-  }
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
 ];
 
-
 const editProfileBtn = document.querySelector(".profile__edit-btn");
-
 const editProfileModal = document.querySelector("#edit-profile-modal");
-
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileFormEl = editProfileModal.querySelector(".modal__form");
 
-editProfileBtn.addEventListener("click", function () {
-  editProfileModal.classList.add("modal_is-open")
-})
-
-editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-open")
-})
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input",
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input",
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
-
 const newPostModal = document.querySelector("#new-post-modal");
-
 const addNewPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const addCardFormElement = newPostModal.querySelector(".modal__form");
+const nameInput = newPostModal.querySelector("#card-caption-input");
+
+const linkInput = newPostModal.querySelector("#card-image-input");
+
+function handleAddCardSubmit(evt) {
+  // Prevent default browser behavior.
+  evt.preventDefault();
+
+  // Log both input values to the console.
+  console.log(nameInput.value);
+  console.log(linkInput.value);
+  // Close the modal.
+  newPostModal.classList.remove("modal_is-open");
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  console.log("FORM SUBMIT WORKING");
+
+  console.log(nameInput);
+
+  console.log(linkInput);
+
+  console.log(nameInput.value);
+
+  console.log(linkInput.value);
+
+  newPostModal.classList.remove("modal_is-open");
+}
+// Create the submit listener.
+addCardFormElement.addEventListener("submit", handleAddCardSubmit);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+// const editProfileNameInput = querySelector("#profile-description-input");
+// const editProfileNameInput = querySelector("#profile-description-input");
+// const profileDescriptionInput = getElementById.card("profile-description-input");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-open");
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
+});
+
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-open");
+});
+
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-open")
-})
+  newPostModal.classList.add("modal_is-open");
+});
 
 addNewPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-open")
-})
+  newPostModal.classList.remove("modal_is-open");
+});
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+
+  editProfileModal.classList.remove("modal_is-open");
+}
+
+editProfileFormEl.addEventListener("submit", handleProfileFormSubmit);
 
 initialCards.forEach(function (card) {
   console.log(card.name);
   console.log(card.link);
-})
+});
